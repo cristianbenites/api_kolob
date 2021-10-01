9361a6088e2cb6f095fff04950eef1e4d06de521
Cristian Benites <cristian.jbenites@gmail.com>
2021-09-30 23:17:58 -0400

    Changes order of Properties table migration. Runs npm install and npm run dev

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+use App\Http\Controllers\PropertyController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
