64c91d7bdc7a0708807ab7fae1feee1825c3a3cb
Cristian Benites <cristian.jbenites@gmail.com>
2021-09-30 22:06:07 -0400

    Adds Jetstream with Inertia and teams to the project

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\PropertyController;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,7 +16,16 @@ use App\Http\Controllers\PropertyController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->name('dashboard');
 
 Route::post('/properties', [PropertyController::class, 'store'])->name('properties.store');
